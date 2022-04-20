9cd93aa68d9993054a8df5ad7b2fb70a85a819f4
JadonJiang <2602775179@qq.com>
2022-04-21 01:41:34 +0800

    温度转换器

---
+++ CesFah.c
@@ -0,0 +1,23 @@
+//打印摄氏度和华氏度的转换表
+
+#include <stdio.h>
+
+int main()
+{
+    int fahr,celsius;
+    int lower,upper,step;
+
+    lower=0;
+    upper=100;
+    step=5;
+    celsius=lower;
+    printf("Celsius-Fahrenheit table\n");
+    while (celsius<=upper)
+    {
+        fahr=celsius*5/9+32;
+        printf("%d\t%d\n",celsius,fahr);
+        celsius=celsius+step;
+    }
+    return 0;
+
+}
